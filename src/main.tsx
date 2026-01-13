bdb95a9d73b20699b4ffb9ad21c5de32135acc89
Giorgi (orb) Orbeliani <g.orbeliani@explorills.com>
2026-01-13 01:10:40 +0000

    feat: add Toaster component for notifications in main application render

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { ErrorBoundary } from "react-error-boundary";
 
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
+import { Toaster } from './components/ui/sonner.tsx'
 
 import "./main.css"
 import "./styles/theme.css"
@@ -11,5 +12,6 @@ import "./index.css"
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
     <App />
+    <Toaster position="top-center" richColors />
    </ErrorBoundary>
 )
